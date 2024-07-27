1563a97f2c07c361fca196053fd299aa5baa8ab3
Developer <chandankumar75707@gmail.com>
2024-07-27 07:01:54 +0530

    code copy from old admin panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'admin-ngx';
+
+
+  Heading:string=''
+  loggedId: boolean = true
+  menuItem = signal<MENU_ITEM[]>(sideMenu)
+  collapsed = signal(false)
+  sideNaveWidth = computed(()=>this.collapsed()?'70px':'210px')
+  Breadcrum(item:string){
+    this.Heading = item;
+  }
 }
